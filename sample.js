ccf553b430b6cff9a537f3b6c2526dee4987fae2
Misato Watanabe <watamisa080513@gmail.com>
2024-08-24 02:12:26 +0900

    todoをcalendarに挿入

--- sample.js
+++ sample.js
@@ -3,7 +3,102 @@ async function getTodo() {
     "https://nu.instructure.com/api/v1/users/self/todo"
   );
   const data = await response.json();
-  return data;
+  return cleansingDatada(data);
+}
+
+function utc2jst(time) {
+  const utcDate = new Date(time);
+
+  // JSTに変換
+  const jstOffset = 9 * 60 * 60 * 1000; // 9時間をミリ秒で計算
+  const jstDate = new Date(utcDate.getTime() + jstOffset);
+
+  return jstDate.toISOString().replace("Z", "+09:00");
+}
+
+function isTimePassed(todoTime) {
+  const nowDate = new Date();
+  const todoTimeDate = new Date(todoTime);
+  return nowDate < todoTimeDate;
+}
+
+function cleansingDatada(data) {
+  return data
+    .filter((d) => isTimePassed(d.assignment.due_at))
+    .map((d) => {
+      const obj = {
+        assignment_id: d.assignment.id,
+        summary: d.assignment.name,
+        description: `${d.context_name}\n${d.assignment.html_url}`,
+        start: {
+          dateTime: utc2jst(d.assignment.due_at),
+          timeZone: "Asia/Tokyo",
+        },
+        end: {
+          dateTime: utc2jst(d.assignment.due_at),
+          timeZone: "Asia/Tokyo",
+        },
+        recurrence: [],
+        attendees: [],
+        reminders: {
+          useDefault: true,
+        },
+      };
+      return obj;
+    });
+}
+
+function getCanvasLmsTodoId(url) {
+  const regex =
+    /https:\/\/nu\.instructure\.com\/courses\/[^\/]+\/assignments\/([^\/]+)/;
+  const match = url.match(regex);
+  if (match && match[1]) {
+    return match[1];
+  } else {
+    return null;
+  }
+}
+
+function getUserEvents() {
+  return new Promise((resolve, reject) => {
+    chrome.runtime.sendMessage(
+      { action: "getUserEvents" },
+      function (response) {
+        if (chrome.runtime.lastError) {
+          reject(chrome.runtime.lastError);
+        } else {
+          resolve(response.data);
+        }
+      }
+    );
+  });
+}
+
+function insertTodo(event) {
+  return new Promise((resolve, reject) => {
+    chrome.runtime.sendMessage(
+      { action: "insertTodo", data: event }
+      // function (response) {
+      // if (chrome.runtime.lastError) {
+      //   reject(chrome.runtime.lastError);
+      // } else {
+      //   resolve(response.data);
+      // }
+      // }
+    );
+  });
+}
+
+async function getRegisteredCanvasLmsEvents() {
+  const userEvents = await getUserEvents();
+  return userEvents
+    .map((e) => {
+      if (e.description) {
+        return getCanvasLmsTodoId(e.description);
+      }
+      return null;
+    })
+    .filter((id) => id);
 }
 
 // ボタンの作成
@@ -13,14 +108,24 @@ button.id = "register-button";
 
 // ボタンにクリックイベントを追加
 button.addEventListener("click", () => {
-  chrome.runtime.sendMessage(
-    {
-      action: "buttonClicked",
-    },
-    function (response) {
-      console.log("response", response);
+  (async () => {
+    const registeredEventIds = await getRegisteredCanvasLmsEvents();
+    const todo = await getTodo();
+    const filteredTodo =
+      registeredEventIds.length === 0
+        ? todo
+        : todo.filter(
+            (e) => !registeredEventIds.includes(String(e.assignment_id))
+          );
+    console.log("filteredTodo", filteredTodo);
+    if (filteredTodo.length === 0) {
+      console.log("nothing todo");
+      return;
     }
-  );
+    filteredTodo.map((todo) => {
+      insertTodo(todo);
+    });
+  })();
 });
 
 let target = document.querySelector("#content"); // 追加したい要素を見つけてくる
--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,55 @@
+const USER_EVENTS =
+  "https://www.googleapis.com/calendar/v3/calendars/primary/events";
+const CANVAS_LMS = "https://nu.instructure.com";
+
+function createUserEventsURL() {
+  const now = new Date();
+  const now_string = now.toISOString();
+  const nextMonth = new Date(now.setMonth(now.getMonth() + 1));
+  return `${USER_EVENTS}?maxResults=100&orderBy=startTime&q=${CANVAS_LMS}&singleEvents=true&timeMin=${now_string}&timeMax=${nextMonth.toISOString()}&`;
+}
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "buttonClicked") {
+  if (message.action === "getUserEvents") {
     chrome.identity.getAuthToken({ interactive: true }, function (token) {
       if (chrome.runtime.lastError || !token) {
         console.error("Failed to get auth token:", chrome.runtime.lastError);
         return;
       }
-      fetch("https://www.googleapis.com/calendar/v3/users/me/calendarList", {
+      fetch(createUserEventsURL(), {
         headers: {
           Authorization: "Bearer " + token,
         },
       })
         .then((response) => response.json())
         .then((data) => {
-          console.log(data, data.items);
+          console.log(data);
           sendResponse({ data: data.items });
+        })
+        .catch((error) => console.error("Error:", error));
+    });
+  }
+  if (message.action === "insertTodo") {
+    chrome.identity.getAuthToken({ interactive: true }, function (token) {
+      if (chrome.runtime.lastError || !token) {
+        console.error("Failed to get auth token:", chrome.runtime.lastError);
+        return;
+      }
+      fetch(USER_EVENTS, {
+        method: "POST",
+        headers: {
+          Authorization: "Bearer " + token,
+          ContentType: "application/json",
+        },
+        body: JSON.stringify(message.data),
+        mode: "cors",
+      })
+        .then((response) => {
+          if (response.status === 200) {
+            return "success";
+          } else {
+            new Error("failed insert event", response);
+          }
         })
         .catch((error) => console.error("Error:", error));
     });
