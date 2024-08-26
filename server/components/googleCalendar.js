4ef85d69b824e282c5d53dd5f48967463722a010
Misato Watanabe <watamisa080513@gmail.com>
2024-08-26 13:34:40 +0900

    ファイルの整理

---
+++ server/components/googleCalendar.js
@@ -0,0 +1,57 @@
+const CANVAS_LMS = "https://nu.instructure.com";
+const USER_EVENTS =
+  "https://www.googleapis.com/calendar/v3/calendars/primary/events";
+
+function createUserEventsURL() {
+  const now = new Date();
+  const now_string = now.toISOString();
+  const nextMonth = new Date(now.setMonth(now.getMonth() + 1));
+  // TODO:prams URLで分ける
+  return `${USER_EVENTS}?maxResults=100&orderBy=startTime&q=${CANVAS_LMS}&singleEvents=true&timeMin=${now_string}&timeMax=${nextMonth.toISOString()}&`;
+}
+
+export function getUserEvents(sendResponse) {
+  chrome.identity.getAuthToken({ interactive: true }, function (token) {
+    if (chrome.runtime.lastError || !token) {
+      console.error("Failed to get auth token:", chrome.runtime.lastError);
+      return;
+    }
+    fetch(createUserEventsURL(), {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        sendResponse({ data: data.items });
+      })
+      .catch((error) => console.error("Error:", error));
+  });
+}
+
+export function insertTodo(data) {
+  chrome.identity.getAuthToken({ interactive: true }, function (token) {
+    if (chrome.runtime.lastError || !token) {
+      console.error("Failed to get auth token:", chrome.runtime.lastError);
+      return;
+    }
+    fetch(USER_EVENTS, {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + token,
+        ContentType: "application/json",
+      },
+      body: JSON.stringify(data),
+      mode: "cors",
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          return "success";
+        } else {
+          new Error("failed insert event", response);
+        }
+      })
+      .catch((error) => console.error("Error:", error));
+  });
+}
--- service-worker.js
+++ service-worker.js
@@ -1,58 +1,19 @@
-const USER_EVENTS =
-  "https://www.googleapis.com/calendar/v3/calendars/primary/events";
-const CANVAS_LMS = "https://nu.instructure.com";
+import {
+  getUserEvents,
+  insertTodo,
+} from "./server/components/googleCalendar.js";
 
-function createUserEventsURL() {
-  const now = new Date();
-  const now_string = now.toISOString();
-  const nextMonth = new Date(now.setMonth(now.getMonth() + 1));
-  return `${USER_EVENTS}?maxResults=100&orderBy=startTime&q=${CANVAS_LMS}&singleEvents=true&timeMin=${now_string}&timeMax=${nextMonth.toISOString()}&`;
-}
+//拡張機能をインストールしたときに呼ばれる
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.alarms.clearAll();
+});
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "getUserEvents") {
-    chrome.identity.getAuthToken({ interactive: true }, function (token) {
-      if (chrome.runtime.lastError || !token) {
-        console.error("Failed to get auth token:", chrome.runtime.lastError);
-        return;
-      }
-      fetch(createUserEventsURL(), {
-        headers: {
-          Authorization: "Bearer " + token,
-        },
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-          sendResponse({ data: data.items });
-        })
-        .catch((error) => console.error("Error:", error));
-    });
+    getUserEvents(sendResponse);
   }
   if (message.action === "insertTodo") {
-    chrome.identity.getAuthToken({ interactive: true }, function (token) {
-      if (chrome.runtime.lastError || !token) {
-        console.error("Failed to get auth token:", chrome.runtime.lastError);
-        return;
-      }
-      fetch(USER_EVENTS, {
-        method: "POST",
-        headers: {
-          Authorization: "Bearer " + token,
-          ContentType: "application/json",
-        },
-        body: JSON.stringify(message.data),
-        mode: "cors",
-      })
-        .then((response) => {
-          if (response.status === 200) {
-            return "success";
-          } else {
-            new Error("failed insert event", response);
-          }
-        })
-        .catch((error) => console.error("Error:", error));
-    });
+    insertTodo(message.data);
   }
   return true;
 });
